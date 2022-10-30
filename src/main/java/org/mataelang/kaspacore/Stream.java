5471c26c09f918dd0c3af5b4a52f93bc49127d87
Fadhil Yori Hibatullah <me@fadhilyori.my.id>
2022-10-30 20:15:14 +0700

    improve some code

--- src/main/java/org/mataelang/kaspacore/Stream.java
+++ src/main/java/org/mataelang/kaspacore/Stream.java
@@ -2,42 +2,18 @@ package org.mataelang.kaspacore;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.functions;
 import org.mataelang.kaspacore.models.AggrSourceIP;
 import org.mataelang.kaspacore.outputs.ConsoleOutput;
 import org.mataelang.kaspacore.outputs.StreamOutput;
-import org.mataelang.kaspacore.schemas.EventSchema;
+import org.mataelang.kaspacore.providers.Spark;
 import org.mataelang.kaspacore.utils.Functions;
-import org.mataelang.kaspacore.utils.PropertyManager;
 
 public class Stream {
     public static void main(String[] args) throws Exception {
-        SparkSession sparkSession = SparkSession
-                .builder()
-                .appName(PropertyManager.getInstance().getProperty("applicationName"))
-                .master(PropertyManager.getInstance().getProperty("sparkMaster"))
-                .config("spark.sql.session.timeZone", "Asia/Jakarta")
-                .getOrCreate();
-
         StreamOutput output = new ConsoleOutput();
+        Dataset<Row> rowDataset = Spark.getSparkKafkaStreamParsed();
 
-        Dataset<Row> rowDataset = sparkSession
-                .readStream()
-                .format("kafka")
-                .option("kafka.bootstrap.servers", PropertyManager.getInstance().getProperty("inputBootstrapServers"))
-                .option("startingOffsets", PropertyManager.getInstance().getProperty("autoOffsetReset"))
-                .option("subscribe", PropertyManager.getInstance().getProperty("outputTopic"))
-                .load();
-
-        Dataset<Row> valueDF =
-                rowDataset.select(
-                        functions.from_json(functions.col("value").cast("string"), EventSchema.getSchema()).alias(
-                                "parsed_value"),
-                        functions.col("timestamp")
-                ).select(functions.col("parsed_value.*"), functions.col("timestamp"));
-
-        Functions.aggregateStream(valueDF, new AggrSourceIP(), output)
+        Functions.aggregateStream(rowDataset, new AggrSourceIP(), output)
                 .start().awaitTermination();
 
     }
--- src/main/java/org/mataelang/kaspacore/providers/Spark.java
+++ src/main/java/org/mataelang/kaspacore/providers/Spark.java
@@ -1,8 +1,13 @@
 package org.mataelang.kaspacore.providers;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.mataelang.kaspacore.schemas.EventSchema;
 import org.mataelang.kaspacore.utils.PropertyManager;
 
 public class Spark {
@@ -25,5 +30,40 @@ public class Spark {
         }
 
         return streamingContext;
+    }
+
+    public static SparkSession getSparkSession() {
+        return SparkSession
+                .builder()
+                .appName(PropertyManager.getInstance().getProperty("applicationName"))
+                .master(PropertyManager.getInstance().getProperty("sparkMaster"))
+                .config("spark.sql.session.timeZone", "Asia/Jakarta")
+                .getOrCreate();
+    }
+
+    public static Dataset<Row> getSparkKafkaStream() {
+        return Spark.getSparkKafkaStream(Spark.getSparkSession());
+    }
+
+    public static Dataset<Row> getSparkKafkaStream(SparkSession sparkSession) {
+        return sparkSession.readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", PropertyManager.getInstance().getProperty("inputBootstrapServers"))
+                .option("startingOffsets", PropertyManager.getInstance().getProperty("autoOffsetReset"))
+                .option("subscribe", PropertyManager.getInstance().getProperty("outputTopic"))
+                .load();
+    }
+
+    public static Dataset<Row> getSparkKafkaStreamParsed() {
+        return Spark.getSparkKafkaStream().select(
+                functions.from_json(
+                        functions.col("value").cast("string"),
+                        EventSchema.getSchema()
+                ).alias("parsed_value"),
+                functions.col("timestamp")
+        ).select(
+                functions.col("parsed_value.*"),
+                functions.col("timestamp").as("event_arrived_time")
+        );
     }
 }
