bba70cec1cc8fc7fe32716228146fa5ed80d79ac
Fadhil Yori Hibatullah <me@fadhilyori.my.id>
2022-11-03 13:33:23 +0700

    modify location field type

--- src/main/java/org/mataelang/kaspacore/utils/IPLookupTool.java
+++ src/main/java/org/mataelang/kaspacore/utils/IPLookupTool.java
@@ -1,6 +1,8 @@
 package org.mataelang.kaspacore.utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.maxmind.db.CHMCache;
 import com.maxmind.geoip2.DatabaseReader;
@@ -60,17 +62,30 @@ public class IPLookupTool {
             return objectNode;
         }
 
-        CountryResponse srcAddrCountry = getInstance().getCountry(objectNode.get("src_addr").textValue());
-        CountryResponse dstAddrCountry = getInstance().getCountry(objectNode.get("dst_addr").textValue());
+        CityResponse srcAddrCountry = getInstance().getCountry(objectNode.get("src_addr").textValue());
+        CityResponse dstAddrCountry = getInstance().getCountry(objectNode.get("dst_addr").textValue());
+        ObjectMapper mapper = new ObjectMapper();
 
         if (srcAddrCountry != null) {
+            ObjectNode srcLocation = mapper.createObjectNode();
+
+            srcLocation.put("lat", srcAddrCountry.getLocation().getLatitude());
+            srcLocation.put("lon", srcAddrCountry.getLocation().getLongitude());
+
             objectNode.put("src_country_code", srcAddrCountry.getCountry().getIsoCode());
             objectNode.put("src_country_name", srcAddrCountry.getCountry().getName());
+            objectNode.set("src_location", srcLocation);
         }
 
         if (dstAddrCountry != null) {
+            ObjectNode dstLocation = mapper.createObjectNode();
+
+            dstLocation.put("lat", dstAddrCountry.getLocation().getLatitude());
+            dstLocation.put("lon", dstAddrCountry.getLocation().getLongitude());
+
             objectNode.put("dst_country_code", dstAddrCountry.getCountry().getIsoCode());
             objectNode.put("dst_country_name", dstAddrCountry.getCountry().getName());
+            objectNode.set("dst_location", dstLocation);
         }
 
         return objectNode;
@@ -109,7 +124,7 @@ public class IPLookupTool {
         return cityResponse;
     }
 
-    public CountryResponse getCountry(String ipAddress) {
+    public CityResponse getCountry(String ipAddress) {
         InetAddress srcAddress;
         try {
             srcAddress = InetAddress.getByName(ipAddress);
@@ -118,9 +133,9 @@ public class IPLookupTool {
             return null;
         }
 
-        CountryResponse countryResponse;
+        CityResponse countryResponse;
         try {
-            countryResponse = IPLookupTool.getInstance().getReader().country(srcAddress);
+            countryResponse = IPLookupTool.getInstance().getReader().city(srcAddress);
         } catch (IOException e) {
             throw new KaspaCoreRuntimeException(e);
         } catch (GeoIp2Exception e) {
