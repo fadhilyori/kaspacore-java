17684f3cb29c29a3f9e66fb8b93b81d0dd514219
Fadhil Yori Hibatullah <me@fadhilyori.my.id>
2022-11-06 21:31:22 +0700

    clean import for Spark.java

--- src/main/java/org/mataelang/kaspacore/jobs/SensorAggregationStreamJob.java
+++ src/main/java/org/mataelang/kaspacore/jobs/SensorAggregationStreamJob.java
@@ -8,21 +8,19 @@ import org.mataelang.kaspacore.models.AggrEvent;
 import org.mataelang.kaspacore.models.AggrSourceIP;
 import org.mataelang.kaspacore.providers.Spark;
 
-import static org.mataelang.kaspacore.providers.Spark.job;
-
 public class SensorAggregationStreamJob {
     public static void main(String[] args) throws Exception {
 
         Dataset<Row> rowDataset = Spark.getSparkKafkaStreamParsed();
 
         // List of jobs
-        job(rowDataset, new AggrAlertInfo()).start();
+        Spark.job(rowDataset, new AggrAlertInfo()).start();
 
-        job(rowDataset, new AggrSourceIP()).start();
+        Spark.job(rowDataset, new AggrSourceIP()).start();
 
-        job(rowDataset, new AggrDestIP()).start();
+        Spark.job(rowDataset, new AggrDestIP()).start();
 
-        job(rowDataset, new AggrEvent()).start();
+        Spark.job(rowDataset, new AggrEvent()).start();
 
         Spark.getSparkSession().streams().awaitAnyTermination();
     }
